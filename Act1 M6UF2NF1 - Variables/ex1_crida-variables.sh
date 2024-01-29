241186d8e0562300fea5ba8502f5b30ad933511d
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-29 09:35:31 +0100

    Update ex1_crida-variables.sh

--- Act1 M6UF2NF1 - Variables/ex1_crida-variables.sh
+++ Act1 M6UF2NF1 - Variables/ex1_crida-variables.sh
@@ -1,1 +1,7 @@
+:'
+Realitza un script que mostri per pantalla les principals variables globals i tots els 
+arguments del Shell, el valor retornat per la darrera funció o comanda i el PID de la Shell actual.
+'
+#!/bin/bash
 
+echo "Variables globals:"
