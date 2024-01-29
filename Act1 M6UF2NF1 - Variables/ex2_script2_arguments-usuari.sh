45398f5addbb0827d296f8e2d8da04efb61721ae
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-29 10:46:02 +0100

    Create ex2_script2_arguments-usuari.sh

---
+++ Act1 M6UF2NF1 - Variables/ex2_script2_arguments-usuari.sh
@@ -0,0 +1,4 @@
+# Realitza un script que mostri un a un (un per línia) - no cal validar-los - els arguments que has introduït i 
+# quants s’han introduït. Pot rebre tants arguments com TU vulguis.
+
+#!/
