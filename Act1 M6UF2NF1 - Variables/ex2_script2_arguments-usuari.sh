fb472a6fc8e749fe825ebee462dc931e87624662
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-29 10:51:05 +0100

    Update ex2_script2_arguments-usuari.sh

--- Act1 M6UF2NF1 - Variables/ex2_script2_arguments-usuari.sh
+++ Act1 M6UF2NF1 - Variables/ex2_script2_arguments-usuari.sh
@@ -1,4 +1,11 @@
 # Realitza un script que mostri un a un (un per línia) - no cal validar-los - els arguments que has introduït i 
 # quants s’han introduït. Pot rebre tants arguments com TU vulguis.
 
-#!/
+#!/bin/bash
+
+echo "S'han introduit $# arguments, que són els següents:"
+
+for arg in "$@"; do
+  echo "$arg"
+done
+  
