3775a61bcbf9cace09e768cee9902b8c5d1e1d58
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-30 08:15:23 +0100

    Update ex2_script3_arguments-usuari.sh

--- Act1 M6UF2NF1 - Variables/ex2_script3_arguments-usuari.sh
+++ Act1 M6UF2NF1 - Variables/ex2_script3_arguments-usuari.sh
@@ -4,7 +4,7 @@
 
 echo "Utilitzant \$* obtenim aquest resultat"
 echo "$*"
-echo "Tots els arguments es mostren com una única cadena."
+echo "Tots els arguments es mostren com una única cadena. (Es conserven espais en blanc i altres caràcters especials)"
 echo "-----------------------------"
 
 echo "Utilitzant \$@ obtenim aquest resultat"
