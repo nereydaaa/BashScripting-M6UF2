9ef5dc6d8d358556f954a7dee5ee839324a0e1fd
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-30 09:46:22 +0100

    Update ex1_script1_cond-operadors.sh

--- Act2 M6UF2NF1 - Condicionals/ex1_script1_cond-operadors.sh
+++ Act2 M6UF2NF1 - Condicionals/ex1_script1_cond-operadors.sh
@@ -1,5 +1,14 @@
 # Realitza un script que rebi un nombre enter per teclat i digui si és positiu o negatiu, comparant-ho amb zero.
 #!/bin/bash
 
-#
+# Demanem el nombre a l'usuari
 read -p "Introdueix un nombre enter: " num
+
+# Comprovem si es positiu o negatiu i mostrem el resultat
+if [num -gt 0]; then
+  echo "El nombre és positiu."
+elif [num -ls 0]; then
+  echo "El nombre és negatiu."
+else
+  echo "El nombre és 0."
+fi
