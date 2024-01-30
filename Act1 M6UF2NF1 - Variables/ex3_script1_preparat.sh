95c2a839820d0832f07597eaf28df1bbad9bb03f
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-30 08:19:22 +0100

    Create ex3_script1_preparat.sh

---
+++ Act1 M6UF2NF1 - Variables/ex3_script1_preparat.sh
@@ -0,0 +1,5 @@
+# Crea un script que demani a l’usuari que introdueixi un nom d’usuari i una contrasenya per, a continuació, mostrar-ho tot en una sola línia.
+# Quan l’usuari introdueixi la seva contrasenya, aquesta no ha d’aparèixer.
+
+#!/bin/bash
+
