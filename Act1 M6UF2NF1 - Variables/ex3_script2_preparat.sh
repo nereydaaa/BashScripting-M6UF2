3f015f3694f947d9e490705b3e21ba8148925c4c
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-01-30 09:33:35 +0100

    Update ex3_script2_preparat.sh

--- Act1 M6UF2NF1 - Variables/ex3_script2_preparat.sh
+++ Act1 M6UF2NF1 - Variables/ex3_script2_preparat.sh
@@ -4,18 +4,27 @@
 
 # En què ens pot beneficiar l’ús dels paràmetres d’expansió? Quin ús els hi puc donar a l’hora de programar Bash scripts?
 
+# Resposta: Els paràmetres d'expansió s'utilitzen per diferents utilitats com poden ser: la manipulació de cadenes de text, iteració a través d'arguments,
+# gestió d'arxius i directoris, substitució de variables, control de flux, etc. Per tant, ens facilita la manipulació de dades i la creació d'scripts més 
+# flexibles i eficients.
+
 #!/bin/bash
 
 CRACK=oriolorioloriol
 echo CRCKMAQUINAFIERAJEFE=$CRACK
 # Identifica què fa el paràmetre ##*
 echo 'El resultat de ##*ori és' ${CRACK##*ori}
+# Elimina la cadena més llarga des del primer ori que troba per la dreta
+
 # Identifica què fa el paràmetre #*
 echo 'El resultat of #*ori és' ${CRACK#*ori}
+# Elimina la cadena més curta des del primer ori que troba per la esquerra
+
 # Identifica què fa el paràmetre %% i/amb el * final
 echo 'El resultat of %%ol* és' ${CRACK%%ol*}
+# Elimina la cadena més llarga des del primer ol que troba per la esquerra
+
 # Proposta de canvi del vostre company i veí Joel
 # Identifica què fa el paràmetre % i/amb el * final
 echo 'El resultat of %ori* és' ${CRACK%ori*}
-
-# Resposta: 
+# Elimina la cadena més curta des del primer ori que troba per la dreta
