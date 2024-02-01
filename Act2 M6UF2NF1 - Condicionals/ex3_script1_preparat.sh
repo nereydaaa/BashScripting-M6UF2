957e939079a74913340817dc4c140d68ec681f39
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-01 10:34:17 +0100

    Update ex3_script1_preparat.sh

--- Act2 M6UF2NF1 - Condicionals/ex3_script1_preparat.sh
+++ Act2 M6UF2NF1 - Condicionals/ex3_script1_preparat.sh
@@ -1,1 +1,19 @@
 # Escriu un script que rebi la ruta d'un fitxer i indiqui els permisos que té (escriptura, lectura, execució).
+#!/bin/bash
+
+# Comprovem si s'ha introduït la ruta com argument
+if [ $# -eq 0 ]; then
+  echo "Error: No s'ha introduït ruta de fitxer."
+  exit 1
+fi
+
+ruta=$1
+
+# Comprovem si existeix
+if [ ! -e "$ruta" ]; then
+  echo "El fitxer no existeix."
+fi
+
+# Obté els permisos del fitxer
+permisos=$(stat -c "%a" "$ruta_del_fitxer")
+echo "$permisos"
