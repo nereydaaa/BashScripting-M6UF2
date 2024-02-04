57c1d7cdde741b0eaa1d212cc8ec5decf757c957
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-04 19:12:28 +0100

    Update ex1_script1_for.sh

--- Act3 M6UF2NF1 - Bucles/ex1_script1_for.sh
+++ Act3 M6UF2NF1 - Bucles/ex1_script1_for.sh
@@ -1,1 +1,6 @@
+# Realitza un script que tregui els comentaris (aquells que comencen per # i arriben fins al final de la línia) de l’arxiu. 
+# Un cop els comentaris ja estiguin eliminats, demana si una paraula/frase existeix dins d’un arxiu. 
+# I un cop feta aquesta comprovació, demana que s’afegeixi una frase al final de l’arxiu (és obligatori que s’introdueixi). 
+
+#!/bin/bash
 
