49cc411c2d66a62eea733ded403d2429ba9aa5b9
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-04 19:26:03 +0100

    Update ex1_script1_for.sh

--- Act3 M6UF2NF1 - Bucles/ex1_script1_for.sh
+++ Act3 M6UF2NF1 - Bucles/ex1_script1_for.sh
@@ -4,3 +4,45 @@
 
 #!/bin/bash
 
+# Comprovem si s'ha pasat la ruta
+if [ $# -eq 0 ]; then
+    echo "Error: No s'ha introduït ruta."
+    exit 1
+fi
+
+arxiu=$1
+
+# Elimina els comentaris de l'ariu
+for linea in $(cat "$arxiu"); do
+    if [[ ! "$linea" =~ ^\# ]]; then
+        echo "$linea" >> temp_file
+    fi
+done
+
+mv temp_file "$arxiu"
+
+# Pregunta a l'usuari la paraula o frase a buscar dins l'ariu
+read -p "Paraula/Frase per buscar a l'ariu: " paraula
+
+# Verifica si apareix
+existeix=false
+for linea in $(cat "$arxiu"); do
+    if [[ "$linea" == *"$paraula"* ]]; then
+        existeix=true
+        break
+    fi
+done
+
+if [ "$existe" = true ]; then
+    echo "La palabra/frase '$paraula' existeix a l'arxiu."
+else
+    echo "La palabra/frase '$paraula' no existeix a l'arxiu."
+fi
+
+# Demana a l'usuari la frase nova
+read -p "Nova frase per introduïr a l'arxiu: " frase
+
+# Agrega la nova frase
+echo "$frase" >> "$arxiu"
+
+echo "S'ha afegit la nova frase a l'arxiu."
