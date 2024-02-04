5a66d530540cd0aa74648a59a72ef345234e9b26
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-04 19:32:38 +0100

    Create ex1_script2_for.sh

---
+++ Act3 M6UF2NF1 - Bucles/ex1_script2_for.sh
@@ -0,0 +1,18 @@
+# Realitza un script que rebi una cadena de nombres enters per teclat i compti quants són zero.
+#!bin/bash
+
+# Demana el nombre a l'usuari
+read -p "Introdueix el nombre enter: " cadena
+
+# Inicialitzem un comptador de 0
+cont=0
+
+# Comptem els 0
+for num in cadena; do
+    if [ "$num" -eq 0 ]; then
+        cont=$((cont + 1))
+    fi
+done
+
+# Mostrem el resultat
+echo "Nombre total de 0: $cont"
