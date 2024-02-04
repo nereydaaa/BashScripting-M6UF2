1a431bc767fc5aae950633e72a76f9224e85a8f3
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-04 20:04:16 +0100

    Create ex2_script2_while.sh

---
+++ Act3 M6UF2NF1 - Bucles/ex2_script2_while.sh
@@ -0,0 +1,4 @@
+# Realitza un script que, inicialment, obtingui els noms de comptes d’usuari del sistema que continguin  alguna lletra majúscula. A continuació, fes que es demani un nom d’usuari. 
+# Mentre el nom introduït no sigui vàlid, mostra un missatge d’error. Un cop el nom introduït sigui vàlid (existent), mostra TOTA la informació disponible  relacionada amb aquest compte d’usuari del sistema.
+#!/bin/bash
+
