aea9c6a159ae999e1df258081da3ddbe5860d202
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-06 10:00:48 +0100

    Create ex4_script2_preparat.sh

---
+++ Act3 M6UF2NF1 - Bucles/ex4_script2_preparat.sh
@@ -0,0 +1,5 @@
+# Crea un script que comprovi si hi ha connexió amb la pàgina web de l'institut per Internet. Mentre no hi hagi connexió es mostrarà un missatge advertint 
+# a l’usuari que no hi ha connexió a Internet i esperarà 5 segons per tornar a comprovar si hi ha connexió.
+# Un cop ja hi hagi connexió, l’script obrirà el navegador Firefox (o el que tu prefereixis) amb la pàgina principal de l’Institut.
+#!/bin/bash
+
