c503bd453f0ffa963c589d924d742b8900741df8
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-08 10:54:38 +0100

    Create ex1_script1_case.sh

---
+++ Act4 M6UF2NF1 - Funcions/ex1_script1_case.sh
@@ -0,0 +1,4 @@
+# Realitza un script que entri en un bucle i que no en surti fins que l’usuari triï si està segur de continuar 
+# avançant en l’espiral d’emocions, alegries i èxits en les que es troba (si continua pots fer que s’obri l’enllaç adjunt). 
+#!/bin/bash
+
