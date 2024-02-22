7395b0afb1ce28a52d8f9fb958bb62ae867d208e
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-22 10:55:54 +0100

    Create ex4_script1_preparat.sh

---
+++ Act4 M6UF2NF1 - Funcions/ex4_script1_preparat.sh
@@ -0,0 +1,2 @@
+# Escriu un script que comprovi l'estat d’un ventall de servidors remots, que es comprovi l'estat de cada servidor i que es registri el resultat en un arxiu. 
+# S’ha d'optimitzar el contingut de l’script a partir de l’ús de funcions. Opcionalment, pots usar Here-Docs i el Case.
