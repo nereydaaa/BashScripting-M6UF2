2e2e0aa3adc79230d1adc5da1676799cc29ceebe
nereydaaa <157602351+nereydaaa@users.noreply.github.com>
2024-02-22 10:25:49 +0100

    Update ex1_script1_case.sh

--- Act4 M6UF2NF1 - Funcions/ex1_script1_case.sh
+++ Act4 M6UF2NF1 - Funcions/ex1_script1_case.sh
@@ -2,3 +2,21 @@
 # avançant en l’espiral d’emocions, alegries i èxits en les que es troba (si continua pots fer que s’obri l’enllaç adjunt). 
 #!/bin/bash
 
+continuar=true
+
+while continuar; do
+    # Demanar confirmació a l'usuario
+    read -p "Estàs segur de continuar avançant en l’espiral d’emocions, alegries i èxits? (s/n): " resposta
+
+    # Verificar la respuesta del usuario
+    case $resposta in
+        [Ss]* ) xdg-open 'https://www.youtube.com/watch?v=jaLDoWqIq2M'; continuar=false;;
+            break ;;
+        [Nn]* ) continuar=false;;
+            echo "Has decidit no continuar. Fins aviat!"
+            exit ;;
+        * ) 
+            echo "Si us plau, respon 's' o 'n'." ;;
+    esac
+done
+
